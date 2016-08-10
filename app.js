9900c5f8a4c221edb0ea32c674d95e14932ddb0d
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-10 15:06:13 -0500

    used a for loop and array

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-// Get three test scores
-var num1 = prompt("Enter first number:", "");
-var num2 = prompt("Enter second number:", "");
-var num3 = prompt("Enter third number:", "");
+// This program averages 3 test scores.
+var num = new Array(3);
 
-var average = (num1 + num2 + num3) / 3;
+// Get three test scores
+for( i=0; i<3; i++) {
+  num[i] = prompt("Enter grade", "");
+}
+
+// Calculate and display the average score
+average = ((parseInt(num[0]) + parseInt(num[1]) + parseInt(num[2])) / 3);
+document.write("Your average is " + average.toFixed(2) + "<br>");
+
+// If the average is grater than or equal to 90, congratulate the user
+if (average >= 90) {
+  document.write("Congratulations!" + "<br>");
+  document.write("<h1>You got an A!<h1>");
+}
