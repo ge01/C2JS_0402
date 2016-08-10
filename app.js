438aab7c1317a1e9fb30a2de6f2c58a434d9b5cf
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-10 14:32:32 -0500

    three test scores

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-function validateFunction() {
-    var x, text;
+// Get three test scores
+var num1 = prompt("Enter first number:", "");
+var num2 = prompt("Enter second number:", "");
+var num3 = prompt("Enter third number:", "");
 
-    // Get the value of the input field with id="numb"
-    x = document.getElementById("num1").value;
-
-    // If x is Not a Number or less than one or greater than 10
-    if (isNaN(x) || x < 1 || x > 100) {
-        text = "Input not valid";
-    } else {
-        text = "Input OK";
-    }
-    document.getElementById("valid-text").innerHTML = text;
-}
+var average = (num1 + num2 + num3) / 3;
