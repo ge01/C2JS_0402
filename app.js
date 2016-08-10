32da3b23de727e291fe154444af92d2849f8bca4
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-10 01:41:18 -0500

    validate function

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-function myFunction() {
+function validateFunction() {
     var x, text;
 
     // Get the value of the input field with id="numb"
-    x = document.getElementById("numb").value;
+    x = document.getElementById("num1").value;
 
     // If x is Not a Number or less than one or greater than 10
-    if (isNaN(x) || x < 1 || x > 10) {
+    if (isNaN(x) || x < 1 || x > 100) {
         text = "Input not valid";
     } else {
         text = "Input OK";
     }
-    document.getElementById("demo").innerHTML = text;
+    document.getElementById("valid-text").innerHTML = text;
 }
